6ee122e04c4a76eb5744c66e834557c1873f42b0
Sarohia94 <sarohia94@gmail.com>
2022-12-05 20:36:07 +0000

    Update css and html for scores. Amend/tidy js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // Counter area for question and score number
-let questionNo = 0;
 let scoreNo = 0;
 
 // Quiz area
@@ -9,113 +8,114 @@ const answersClass = document.getElementsByClassName("answers")
 // End area
 const endScore = document.getElementById("end-score")
 const yourName = document.getElementById("name")
-const submitScore = document.getElementById("submit")
+const submitScore = document.getElementById("submit-score")
 
 // scoreboard area
 const highScoresList = document.getElementById("scores-list")
 
 // Questions array
 const myQuestions = [{
-        question: "What year did WW1 start?",
-        answers: {
-            answer1: "1912",
-            answer2: "1914",
-            answer3: "1918",
-            answer4: "1939"
-        },
-        correctAnswer: "answer2"
+    question: "What year did WW1 start?",
+    answers: {
+        answer1: "1912",
+        answer2: "1914",
+        answer3: "1918",
+        answer4: "1939"
     },
-    {
-        question: "Who was the British Prime Minister during the Falklands War?",
-        answers: {
-            answer1: "John Major",
-            answer2: "Tony Blair",
-            answer3: "Margaret Thatcher",
-            answer4: "Harold Wilson"
-        },
-        correctAnswer: "answer3"
+    correctAnswer: "answer2"
+},
+{
+    question: "Who was the British Prime Minister during the Falklands War?",
+    answers: {
+        answer1: "John Major",
+        answer2: "Tony Blair",
+        answer3: "Margaret Thatcher",
+        answer4: "Harold Wilson"
     },
-    {
-        question: "What year was the Cuban Missile Crisis?",
-        answers: {
-            answer1: "1952",
-            answer2: "1962",
-            answer3: "1964",
-            answer4: "1948"
-        },
-        correctAnswer: "answer2"
+    correctAnswer: "answer3"
+},
+{
+    question: "What year was the Cuban Missile Crisis?",
+    answers: {
+        answer1: "1952",
+        answer2: "1962",
+        answer3: "1964",
+        answer4: "1948"
     },
-    {
-        question: "Who was the Prime Minister at the outbreak of the Boer War?",
-        answers: {
-            answer1: "Lord Churchill",
-            answer2: "Lord Salisbury",
-            answer3: "Lord Aberdeen",
-            answer4: "Lord Gladstone"
-        },
-        correctAnswer: "answer2"
+    correctAnswer: "answer2"
+},
+{
+    question: "Who was the Prime Minister at the outbreak of the Boer War?",
+    answers: {
+        answer1: "Lord Churchill",
+        answer2: "Lord Salisbury",
+        answer3: "Lord Aberdeen",
+        answer4: "Lord Gladstone"
     },
-    {
-        question: "What year did the Battle of the Somme take place?",
-        answers: {
-            answer1: "1942",
-            answer2: "1916",
-            answer3: "1917",
-            answer4: "1940"
-        },
-        correctAnswer: "answer2"
+    correctAnswer: "answer2"
+},
+{
+    question: "What year did the Battle of the Somme take place?",
+    answers: {
+        answer1: "1942",
+        answer2: "1916",
+        answer3: "1917",
+        answer4: "1940"
     },
-    {
-        question: "Which of the below was not part of the Russian Civil War 1917-1923?",
-        answers: {
-            answer1: "Red Army",
-            answer2: "Blue Army",
-            answer3: "Green Army",
-            answer4: "White Army"
-        },
-        correctAnswer: "answer2"
+    correctAnswer: "answer2"
+},
+{
+    question: "Which of the below was not part of the Russian Civil War 1917-1923?",
+    answers: {
+        answer1: "Red Army",
+        answer2: "Blue Army",
+        answer3: "Green Army",
+        answer4: "White Army"
     },
-    {
-        question: "What is the name of the peacekeeping organisation formed after WWII?",
-        answers: {
-            answer1: "United Nations",
-            answer2: "NATO",
-            answer3: "Warsaw Pact",
-            answer4: "Partnership for Peace"
-        },
-        correctAnswer: "answer1"
+    correctAnswer: "answer2"
+},
+{
+    question: "What is the name of the peacekeeping organisation formed after WWII?",
+    answers: {
+        answer1: "United Nations",
+        answer2: "NATO",
+        answer3: "Warsaw Pact",
+        answer4: "Partnership for Peace"
     },
-    {
-        question: "The Tuskegee Airmen constituted a U.S. Army unit drawn from which group?",
-        answers: {
-            answer1: "African Americans",
-            answer2: "Women",
-            answer3: "WW1 Veterans",
-            answer4: "Youths under age 18"
-        },
-        correctAnswer: "answer1"
+    correctAnswer: "answer1"
+},
+{
+    question: "The Tuskegee Airmen constituted a U.S. Army unit drawn from which group?",
+    answers: {
+        answer1: "African Americans",
+        answer2: "Women",
+        answer3: "WW1 Veterans",
+        answer4: "Youths under age 18"
     },
-    {
-        question: "When was the Korean Armistice Agreement signed?",
-        answers: {
-            answer1: "1951",
-            answer2: "1950",
-            answer3: "1952",
-            answer4: "1953"
-        },
-        correctAnswer: "answer4"
+    correctAnswer: "answer1"
+},
+{
+    question: "When was the Korean Armistice Agreement signed?",
+    answers: {
+        answer1: "1951",
+        answer2: "1950",
+        answer3: "1952",
+        answer4: "1953"
     },
-    {
-        question: "How long did the Bosnian War last?",
-        answers: {
-            answer1: "3 years",
-            answer2: "4 years",
-            answer3: "5 years",
-            answer4: "2 years"
-        },
-        correctAnswer: "answer1"
-    }
+    correctAnswer: "answer4"
+},
+{
+    question: "How long did the Bosnian War last?",
+    answers: {
+        answer1: "3 years",
+        answer2: "4 years",
+        answer3: "5 years",
+        answer4: "2 years"
+    },
+    correctAnswer: "answer1"
+}
 ];
+
 let currentQuestionIndex = 0;
 
 // Shuffle variables - https://www.youtube.com/watch?v=riDzcEQbX6k
@@ -131,15 +131,15 @@ function startQuiz() {
 }
 
 function setNextQuestion() {
-    if (currentQuestionIndex <= 9) {
+    if (currentQuestionIndex <= myQuestions.length - 1) {
         displayQuestion(shuffleQuestions[currentQuestion]);   
     } else {
         document.getElementById("quiz-area").classList.add("hide");
         document.getElementById("end-area").classList.remove("hide");
         endScore.innerText = `${scoreNo}/10`;
     }
-    let previousQuestion = parseInt(document.getElementById("q-number").innerText);
-    document.getElementById("q-number").innerText = ++previousQuestion;
+    let previousQuestionNo = parseInt(document.getElementById("q-number").innerText);
+    document.getElementById("q-number").innerText = ++previousQuestionNo;
     // console.log(previousQuestion); // check
 }
 
@@ -174,10 +174,12 @@ function incrementScore() {
 }
 
 startQuiz();
+// console.log("start funct called");
 
-// Save scores https://www.youtube.com/watch?v=DFhmNLKwwGw&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=10
+// Save scores - https://www.youtube.com/watch?v=DFhmNLKwwGw&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=10
+
 function saveScore(e) {
-    // prevent form opening on a new page
+    // prevent form posting on a new page
     e.preventDefault();
     // log object to hold name & high score
     const log = {
@@ -187,20 +189,22 @@ function saveScore(e) {
     // push log object into high scores list
     highScores.push(log);
     // sorts score by value
-    highScores((a, b) => b.score - a.score);
+    highScores.sort((a, b) => b.score - a.score);
     // sets high score list as 5
     highScores.splice(5);
     // update high score in local storage
     localStorage.setItem("highScores", JSON.stringify(highScores));
     // opens the scoreboard.html
-    window.location.assign("scoreboard.html")
+    window.location.assign("scoreboard.html");
 }
 
 // retrieve high scores list or an empty array
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 // add event listener when adding name to submit score. Where there is no name the submit button is disabled.
-yourName.addEventListener("keyup", () => {submitScore.disabled = !yourName.value;});
+yourName.addEventListener("keyup", () => {
+    submitScore.disabled = !yourName.value;
+});
 
 // add event listener on submit score to call the save score function
 submitScore.addEventListener("click", saveScore);
